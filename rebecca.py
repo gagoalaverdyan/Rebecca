1a691aaa5b11850d6a71a155537384d88ab4f6a9
gagoalaverdyan <gagoalaverdyan@gmail.com>
2023-12-06 01:24:50 +0400

    Implemented all the basic fucntions

--- rebecca.py
+++ rebecca.py
@@ -5,11 +5,11 @@ from .rebecca.mainfunctions import *
 
 
 def main():
-    print("Welcome to Rebecca")  # Add short intro afetr and add colors
-    choice = input("Enter 'e' to encrypt or 'd' to decrypt:\n")  # need to add checks
-    message = input("Enter the message:\n")  # need to add checks
-    shift = input("Enter the shift value (1-366):\n")  # need to add checks
-    crypt_file = input("Enter the filename with extension (e.g. crypto.txt):\n")
+    welcome_message()
+    choice = make_choice()  # Add colors, q to exit
+    message = enter_message()  # Add colors, q to exit
+    shift = enter_shift()  # Add colors, q to exit
+    crypt_file = get_cipher_file()  # Add colors, q to exit
 
     text = load_file(crypt_file)
     char_dict = make_dict(text, shift)
--- rebecca/mainfunctions.py
+++ rebecca/mainfunctions.py
@@ -1,11 +1,53 @@
 import os
 from collections import Counter, defaultdict
 
+# Basic things are implemented as functions to avoid errors from the user's side and to make the code more readable.
+
+
+def welcome_message():
+    """Print a welcome message for the program."""
+    print("Welcome to Rebecca")  # Add short intro after and add colors,
+
+
+def make_choice():
+    """Receive user's intention to encrypt or decrypt the message and return it."""
+    choice = input("Enter 'e' to encrypt or 'd' to decrypt:\n")
+    while choice not in ("d", "e"):
+        choice = input("Wrong option. Enter 'e' to encrypt or 'd' to decrypt:\n")
+    if choice == "e":
+        return "encrypt"
+    else:
+        return "decrypt"
+
+
+def enter_message():
+    """Receive user's message to encrypt or decrypt and return it."""
+    message = str(input("Enter the message:\n"))
+    while not message:
+        message = input("No input received.\nEnter the message:\n")
+    return message
+
+
+def enter_shift():
+    """Receive a number in the range 1-365 to simulate the daily shift and encrypt or decrypt characters."""
+    shift = input("Enter the shift value (1-365):\n")
+    while not shift or type(shift) != int or not 1 <= shift <= 365:
+        shift = input("The shift value must be an integer in the range 1-365:\n")
+    return shift
+
+
+def get_cipher_file():
+    """Receive the name of the file to be used as the cipher and return it."""
+    filename = str(
+        input("Enter the cipher filename with extension (e.g. crypto.txt):\n")
+    )
+    while not filename or filename[-4:] != ".txt" or not os.path.exists(filename):
+        filename = input("Enter a valid .txt file name:\n")
+    return filename
+
 
 def load_file(filename):
     """Read the given file and return it as a string of lowercase characters."""
-    while not filename or filename[-4:] != "txt" or not os.path.exists(filename):
-        filename = input("Enter a valid .txt file name:\n")
     with open(filename, encoding="utf-8", errors="ignore") as file:
         file_string = file.read().lower()
     return file_string
